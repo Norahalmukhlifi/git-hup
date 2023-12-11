1c314790c34882d13d58b4fed43727eecdd60f80
Norahalmukhlifi <norahalmokhlifi@gmail.com>
2023-12-11 10:31:19 +0300

    Initial Commit

---
+++ git-hup/git_hupApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_hupApp.swift
+//  git-hup
+//
+//  Created by NorahAlmukhlifi on 27/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct git_hupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
